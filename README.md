22ac43cff2912cfc13cf07b7560474b0ed5e56bc
Shaw <x13945@foxmail.com>
2018-01-27 15:16:38 +0800

    完成基本的拉取issue列表功能

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # fetch_gitlab_issue
-由于GitLab没有导出issue的功能，因此写个小工具用于拉取GitLab中的issue列表
+由于GitLab没有导出issue的功能，因此写个小工具用于拉取GitLab中的issue列表。
+当前脚本基于Gitlab v3版本的api，把issue列表导出到json文件中，而json文件可以很方便的转换成其他的文件格式，如xml、csv、excel等等。
+
+## 环境
+当前脚本需要运行在**Python3**环境下
+
+## 使用
+使用该脚本后，issue会输出到脚本所在目录下的叫做`issues.json`的文件中。
+使用格式：
+```shell
+./fetchGitlabIssue.py http://host_to_gitlab "private token"
+或
+python3 ./fetchGitlabIssue.py http://host_to_gitlab "private token"
+```
+
+例如，我的gitlab是部署在192.168.8.1这台机器上，平时我通过`http://192.168.8.1`访问我gitlab，通过`http://192.168.8.1/profile/personal_access_tokens`配置assess token。那我用的时候，就这么用：
+```shell
+./fetchGitlabIssue.py http://192.168.8.1 "我的token"
+```
+
+## TODO
+当前脚本比较简陋，只能称作可以使用而已，后续有机会（？）会做出改进。
---
+++ fetchGitlabIssue.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+import requests
+import sys
+import json
+print(len(sys.argv))
+print(sys.argv)
+if len(sys.argv) < 3:
+    errorLog = """
+    请输入正确的参数，例如：\n
+    fetchGitlabIssue.py http://host_to_gitlab "private token"
+    """
+    print(errorLog)
+    sys.exit(0)
+
+host = sys.argv[1]
+privateToken = sys.argv[2]
+
+fetchGroupsUrl = "%s/api/v3/groups?private_token=%s" % (host, privateToken)
+response = requests.get(fetchGroupsUrl)
+groups = json.loads(response.text)
+print(json.dumps(groups, indent=2, sort_keys=True)
+      + '\n请选择一个Group Id用于拉取Issue列表:\n\n')
+
+print("\tGroupId\t\tGroupName")
+for group in groups:
+    print("\t%d\t\t%s" % (group['id'], group['full_name']))
+groupId = input("请输入：\n")
+
+fetchProjectsUrl = "%s/api/v3/groups/%s/projects?private_token=%s" % (host, groupId, privateToken)
+response = requests.get(fetchProjectsUrl)
+projects = json.loads(response.text)
+print(json.dumps(projects, indent=2, sort_keys=True)
+      + '\n请选择一个Project Id用于拉取Issue列表:\n\n')
+
+print("\tProjectId\t\tProjectName")
+for project in projects:
+    print("\t%d\t\t%s" % (project['id'], project['name']))
+projectId = input("请输入：\n")
+
+fetchIssuesUrl = "%s/api/v3/projects/%s/issues?private_token=%s" % (host, projectId, privateToken)
+issues = json.loads(requests.get(fetchIssuesUrl).text)
+print(json.dumps(issues, indent=2, sort_keys=True))
+
+with open('issues.json', 'w') as issuesFile:
+    json.dump(issues, issuesFile)
+
+print("Issues 已经被写入issues.json")
